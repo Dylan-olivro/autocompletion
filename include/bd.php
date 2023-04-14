2601198ee2ec7f54b3c62d280b87125669fee921
dylan-olivro <dylan.olivro@laplateforme.io>
2023-04-14 12:35:37 +0200

    fix bug

--- include/bd.php
+++ include/bd.php
@@ -3,9 +3,12 @@ session_start();
 $servername = 'localhost';
 $username = 'root';
 $password = '';
+$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+$pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES utf8";
+
 
 try {
-    $bdd = new PDO("mysql:host=$servername;dbname=autocompletion", $username, $password);
+    $bdd = new PDO("mysql:host=$servername;dbname=autocompletion", $username, $password, $pdo_options);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo 'Connexion réussie';
 } catch (PDOException $e) {
